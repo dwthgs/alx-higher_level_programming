df119fa494088a001dc16242a307b32e5d32de15
dev <dev@devs.local>
2021-07-13 23:47:52 +0300

    sql

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,5 +1,4 @@
---
---
+-- uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexter
 SELECT DISTINCT  tv_genres.name 
 FROM tv_shows INNER JOIN tv_show_genres 
 ON tv_shows.id = tv_show_genres.show_id 
--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,5 +1,4 @@
---
---
+-- lists all shows without the genre Comedy in the database hbtn_0d_tvshows
 SELECT DISTINCT  tv_shows.title 
 FROM tv_shows LEFT OUTER JOIN tv_show_genres 
 ON tv_shows.id = tv_show_genres.show_id 
