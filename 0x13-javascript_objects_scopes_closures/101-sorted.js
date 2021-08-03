62b89260dfea72a7f283bbaf76b00beea7d248e9
dev <dev@devs.local>
2021-08-03 23:37:06 +0300

    -

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 const dict = require('./101-data.js').dict;
 const newDict = {};
-for (const key in dict)
+for (const key in dict) {
   if (newDict[dict[key]] === undefined) newDict[dict[key]] = [key];
   else newDict[dict[key]].push(key);
+}
 console.log(newDict);
