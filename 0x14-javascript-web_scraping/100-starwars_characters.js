b9d2382185d0739748f9fbc0f354159b023b2b50
dev <dev@devs.local>
2021-09-16 01:55:47 +0300

    -

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,13 +1,14 @@
 #!/usr/bin/node
 
-const request = require("request");
-const url = "https://swapi-api.hbtn.io/api/films/" + process.argv[2];
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
 request(url, function (err, res, data) {
-  if (!err)
+  if (!err) {
     JSON.parse(data).characters.map((character) =>
       request(character, (err, res, data) => {
         if (!err) console.log(JSON.parse(data).name);
       })
     );
+  }
 });
--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 
-const request = require("request");
-const url = "https://swapi-api.hbtn.io/api/films/" + process.argv[2];
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
 request(url, (err, res, body) => {
   if (!err) printName(JSON.parse(body).characters, 0);
 });
 
-function printName(characters, i) {
+function printName (characters, i) {
   request(characters[i++], function (err, res, data) {
     if (!err) {
       console.log(JSON.parse(data).name);
