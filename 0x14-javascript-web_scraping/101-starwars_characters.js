039574d633421375cab2dc8cebe7c89a0a5e2126
dev <dev@devs.local>
2021-09-16 00:40:04 +0300

    -

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,28 +1,15 @@
 #!/usr/bin/node
-let request = require('request');
-let episodeNumber = process.argv[2];
-let url = 'http://swapi.co/api/films/' + episodeNumber;
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else if (response.statusCode === 200) {
-    let charDict = {};
-    let allChars = JSON.parse(body).characters;
-    for (let c in allChars) {
-      request(allChars[c], function (err, response, body) {
-        if (err) {
-          console.log(err);
-        } else {
-          charDict[c] = JSON.parse(body).name;
-        }
-        if (allChars.length === Object.keys(charDict).length) {
-          for (let key in charDict) {
-            console.log(charDict[key]);
-          }
+const request = require("request");
+const url = "https://swapi-api.hbtn.io/api/people/" + process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    characters.forEach((character) => {
+      request(character, function (error, response, body) {
+        if (!error) {
+          console.log(JSON.parse(body).name);
         }
       });
-    }
-  } else {
-    console.log('Wrong status code');
+    });
   }
 });
